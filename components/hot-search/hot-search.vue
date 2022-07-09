bec345d2cfddd01c49b1da935576993bab37a211
“wei <116019284@qq.com>
2022-07-09 16:22:56 +0800

    搜索模块search

---
+++ components/hot-search/hot-search.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="my-search-container">
+		<!-- 搜索按钮 -->
+		<view class="my-search-box">
+			<image class="icon" src="@/static/images/search.png" />
+			<text class="placeholder">{{ placeholderText }}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'hot-search',
+	props: {
+		placeholderText: {
+			type: String,
+			default: '搜索'
+		}
+	},
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.my-search-container {
+	display: flex;
+	align-items: center;
+	.my-search-box {
+		height: 36px;
+		width: 100%;
+		background-color: #ffffff;
+		border-radius: 15px;
+		border: 1px solid #c9c9c9;
+		display: flex;
+		align-items: center;
+		padding: 0 10px;
+		.icon {
+			width: 15px;
+			height: 15px;
+		}
+		.placeholder {
+			font-size: 10px;
+			margin-left: 10px;
+			color: #454545;
+		}
+	}
+}
+</style>
--- pages/hot/hot.vue
+++ pages/hot/hot.vue
@@ -2,6 +2,10 @@
 	<view class="hot-container">
 		<!-- logo -->
 		<image class="logo" src="@/static/images/logo.png" mode="aspectFit"></image>
+		<view class="search-box">
+			<!-- 搜索模块 -->
+			<hot-search placeholderText="uni-app 自定义组件" />
+		</view>
 	</view>
 </template>
 
@@ -19,6 +23,10 @@ export default {
 	.logo {
 		width: 100%;
 		height: 80px;
+	}
+	.search-box {
+		padding: 0 16px;
+		margin-bottom: $uni-spacing-col-base;
 	}
 }
 </style>
