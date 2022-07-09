32cce2a9a3ff257421d219e42f7c36b912fc98df
“wei <116019284@qq.com>
2022-07-09 15:13:53 +0800

    tabbar导航栏

---
+++ pages/hot-video/hot-video.vue
@@ -0,0 +1,13 @@
+<template>
+	<view>hot-video</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss"></style>
---
+++ pages/hot/hot.vue
@@ -0,0 +1,13 @@
+<template>
+	<view>hot</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss"></style>
