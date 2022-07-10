f63d245003ae342d0dc5085bded4a7fc750bd3da
“wei <116019284@qq.com>
2022-07-10 13:12:59 +0800

    feat: tabs组件 - 增加可配置项

--- components/hot-tabs/hot-tabs.vue
+++ components/hot-tabs/hot-tabs.vue
@@ -43,23 +43,18 @@
 export default {
 	name: 'hot-tabs',
 	props: {
-		// 1.可以在父组件中定制样式
-		// 配置对象
 		config: {
 			type: Object,
-			// default 如果是引用数据类型， 那么需要指定 value为一个函数， 通过 返回值的形式 执行默认值
 			default: () => {
 				return {};
 			}
 		},
-		// 2.可以在父组件中指定数据
 		tabData: {
 			type: Array,
 			default: () => {
 				return [];
 			}
 		},
-		// 3.可以在父组件中设置默认激活项
 		defaultIndex: {
 			type: Number,
 			default: 0
@@ -67,28 +62,17 @@ export default {
 	},
 	data() {
 		return {
-			// 当前激活项的 index
 			activeIndex: -1,
-			// 滑块
-			// 想要实现滑块的滚动：
-			// 1.确定滚动的时机 -> 点击tab时
-			// 2.计算滑块滚动的距离
 			slider: {
 				left: 0
 			},
-			// 内部维护的数据对象，为每个 item 单独额外维护一个 slider 的滑块对象
 			tabList: [],
 			// 默认配置
 			defaultConfig: {
-				// 默认的字体颜色
 				textColor: '#333333',
-				// 高亮字体颜色
 				activeTextColor: '#f94d2a',
-				// 下划线宽度 px
 				underLineWidth: 24,
-				// 下划线高度 px
 				underLineHeight: 2,
-				// 下划线颜色
 				underLineColor: '#f94d2a'
 			},
 			// scrollView 的横向滚动条位置
@@ -111,11 +95,6 @@ export default {
 			const underLineWidth = this.defaultConfig.underLineWidth;
 			// 配置 滚动条 的数据
 			this.slider = {
-				// TODO：left 如何定义呢？
-				// 1. 维护一个单独的数据对象 `tabList`
-				// 2. 在 `tabList`  的 `item` 中为一个 `_slider` 属性
-				// 3. 该属性保存了 【当前 `item` 下 的滑块位置】
-				//    3.1. 计算公式：`滑块左侧位置 = item.left + (item.width - slider.width) / 2`
 				left: this.tabList[activeIndex]._slider.left
 			};
 			// 控制scrollView 进行横向的滚动
@@ -134,9 +113,9 @@ export default {
 				query
 					.select('#_tab_' + index)
 					.boundingClientRect(res => {
-						// 为数据对象中每一个 item 都维护一个 _slider（滑动条） 对象
+						//  新增一个 `_slider` 属性
 						item._slider = {
-							// 当前的 tab 距离左侧的距离
+							// 当前的 tab 距离左侧的距离, 计算公式：`滑块左侧位置 = item.left + (item.width - slider.width) / 2`
 							left: res.left + (res.width - this.defaultConfig.underLineWidth) / 2
 						};
 						// 运算完成之后，执行一次 【滑块】位置运算
@@ -149,7 +128,6 @@ export default {
 		}
 	},
 	watch: {
-		// 监听 tabData 的变化
 		tabData: {
 			// 当 tabData 发生变化时, 回调的方法
 			handler(val) {
@@ -161,20 +139,16 @@ export default {
 			// 当前的 handler 回调将会在侦听开始之后被立即调用
 			immediate: true
 		},
-		// 监听激活项目的变化
 		defaultIndex: {
 			handler(val) {
 				this.activeIndex = val;
 			},
-			// 该回调将会在侦听开始之后被立即调用
 			immediate: true
 		},
-		// 监听 config
 		config: {
 			handler(val) {
 				this.defaultConfig = { ...this.defaultConfig, ...val };
 			},
-			// 该回调将会在侦听开始之后被立即调用
 			immediate: true
 		}
 	}
